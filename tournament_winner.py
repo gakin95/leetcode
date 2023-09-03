2713e9bc3c5a11429caceac71a0a5e2c6bc7f094
gakin95 <gakin95@gmail.com>
2023-09-03 14:03:57 -0500

    tournament winner

--- tournament_winner.py
+++ tournament_winner.py
@@ -1,3 +1,24 @@
 def tournamentWinner(competitions, results):
     # Write your code here.
-    return ""
+    dic = {}
+
+    for opponents_ids, opponents in enumerate(competitions):
+        winner = results[opponents_ids]
+        pos = 0
+        if winner == 0:
+            pos = 1
+        if opponents[pos] in dic:
+            dic[opponents[pos]] += 1
+        else:
+            dic[opponents[pos]] = 1
+
+    max_key = max(dic, key=dic.get)
+    return max_key
+
+competitions = [
+    ["HTML","C#"],
+    ["C#","Python"],
+    ["Python","HTML"]
+]
+results = [0,0,1]
+print(tournamentWinner(competitions, results))
