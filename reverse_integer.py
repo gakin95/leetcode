0334a889809db7498b518fdf03009aa3bfc994e7
gakin95 <gakin95@gmail.com>
2023-09-04 18:57:58 -0500

    Reverse Integer

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,12 +1,16 @@
-import numpy as np
+class Solution:
+    def reverse(self, x: int) -> int:
+        y = str(abs(x))
+        z = []
+        w = 1
+        if x < 0:
+            w = -1
+        for char in reversed(y):
+            z.append(char)
 
+        reversed_int = w * int("".join(z))
 
-def reverse(x: int) -> int:
-    y = str(abs(x))
-    z = []
-    for char in reversed(y):
-        z.append(char)
-    print(z)
-    return np.sign(x) * int("".join(z))
+        if reversed_int < -2 ** 31 or reversed_int > 2 ** 31 - 1:
+            return 0
 
-print(reverse(123))
+        return reversed_int
