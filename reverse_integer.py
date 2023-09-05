7f7a781860b606e361104b560b719e9a05ed4d93
gakin95 <gakin95@gmail.com>
2023-09-04 19:02:42 -0500

    Reverse Integer

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,16 +1,31 @@
+# class Solution:
+#     def reverse(self, x: int) -> int:
+#         y = str(abs(x))
+#         z = []
+#         w = 1
+#         if x < 0:
+#             w = -1
+#         for char in reversed(y):
+#             z.append(char)
+#
+#         reversed_int = w * int("".join(z))
+#
+#         if reversed_int < -2 ** 31 or reversed_int > 2 ** 31 - 1:
+#             return 0
+#
+#         return reversed_int
+
 class Solution:
     def reverse(self, x: int) -> int:
-        y = str(abs(x))
-        z = []
-        w = 1
+        # Handle negative numbers by multiplying with -1 after reversing
         if x < 0:
-            w = -1
-        for char in reversed(y):
-            z.append(char)
+            reversed_int = -int(str(-x)[::-1])
+        else:
+            reversed_int = int(str(x)[::-1])
 
-        reversed_int = w * int("".join(z))
-
+        # Check if reversed_int is outside the 32-bit signed integer range
         if reversed_int < -2 ** 31 or reversed_int > 2 ** 31 - 1:
             return 0
 
         return reversed_int
+
