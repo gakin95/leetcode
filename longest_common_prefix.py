72312a19d0aa22a82315af5d4f8c343e8c10a4e7
gakin95 <gakin95@gmail.com>
2023-09-05 23:55:33 -0500

    longestCommonPrefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,17 +1,47 @@
 from typing import List
 
 
-def longestCommonPrefix(strs: List[str]) -> str:
+# def longestCommonPrefix(strs: List[str]) -> str:
+#     if not strs:
+#         return ""
+#     min_string = min(strs, key=len)
+#     for i, char in enumerate(min_string):
+#         for str in strs:
+#             if str[i] != char:
+#                 return min_string[:i]
+#     return min_string
+
+def longestCommonPrefix(strs):
     if not strs:
         return ""
-    min_string = min(strs, key=len)
-    for i, char in enumerate(min_string):
-        for str in strs:
-            if str[i] != char:
-                return min_string[:i]
-    return min_string
+
+    # Find the string with the smallest length
+    minStr = min(strs, key=len)
+
+    # Setting the range for binary search
+    low, high = 0, len(minStr)
+
+    while low <= high:
+        mid = (low + high) // 2
+        if isCommonPrefix(strs, mid):
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    # Return the common prefix of length (low - 1)
+    return minStr[:low - 1]
 
 
-strs = ["flower","flow","flight"]
+def isCommonPrefix(strs, length):
+    prefix = strs[0][:length]
+    for i in range(1, len(strs)):
+        if not strs[i].startswith(prefix):
+            return False
+    return True
 
-print(longestCommonPrefix(strs))
+
+# Test
+strs = ["flower", "flow", "flight"]
+print(longestCommonPrefix(strs))  # Outputs: "fl"
+
+
