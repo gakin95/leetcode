7a7b2c0ca64f71f1146a57148a4a383b6d29fbd1
gakin95 <gakin95@gmail.com>
2023-09-08 13:59:09 -0500

    stack

--- letter_combinations.py
+++ letter_combinations.py
@@ -2,5 +2,36 @@ from typing import List
 
 
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        return ""
+    def letterCombinations(self, digits: str):
+        if not digits:
+            return []
+
+        mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        def backtrack(combination, next_digits):
+            # if there is no more digits to check
+            if len(next_digits) == 0:
+                # the combination is done
+                output.append(combination)
+            else:
+                # iterate over all letters which map the next available digit
+                for letter in mapping[next_digits[0]]:
+                    # append the current letter to the combination
+                    # and proceed to the next digits
+                    backtrack(combination + letter, next_digits[1:])
+
+        output = []
+        backtrack("", digits)
+        return output
+
+output = Solution()
+print(output.letterCombinations("2"))
