69d4baa3f4d5b76dc9ce726b4510124e6567c466
gakin95 <gakin95@gmail.com>
2023-09-08 18:01:20 -0500

    Valid Parentheses

--- stack.py
+++ stack.py
@@ -41,6 +41,22 @@ def is_balanced_parentheses(string):
             stack_list.append(char)
     return len(stack_list) == 0
 
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else "#"
+                if mapping[char] != top:
+                    return False
+            else:
+                stack.append(char)
+        return len(stack) == 0
+
+
 my_stack = Stack()
 my_stack.push(1)
 my_stack.push(2)
